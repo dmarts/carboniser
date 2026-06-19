c47bb41e91b0c5cd3eee3456ea84c7a485f3dbaa
Dan Martyr <47741043+dmarts@users.noreply.github.com>
2026-06-19 14:51:35 +0100

    Initial Commit

---
+++ carboniser/carboniserApp.swift
@@ -0,0 +1,17 @@
+//
+//  carboniserApp.swift
+//  carboniser
+//
+//  Created by Dan M on 19/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct carboniserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
